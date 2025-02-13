59528dba5e508aaf76b91b6884fd594c12e694b8
FARWA BATOOL <162696102+FB-Github-A@users.noreply.github.com>
2025-02-13 15:53:00 +0500

    Update importData.js

--- scripts/importData.js
+++ scripts/importData.js
@@ -1,11 +1,11 @@
-import { createClient } from '@sanity/client';
+cimport { createClient } from '@sanity/client';
 
 const client = createClient({
-  projectId: 'u15rxwv2',
-  dataset: 'production',
+  projectId: '',
+  dataset: '',
   useCdn: true,
   apiVersion: '2025-01-13',
-  token: 'sk73QSe1r6oV9SiY2xs9ioWKmW6huYTii92M8Bgmxnp5YR1I1rdUnle6F4AwsK5pVdVx12xVxkPwFhmMb6q4walF1Je5cd4zev0oojYprX972BT63QMtLsTL2coa4nc5s3hjz2buiV0xgHU0MkpSLkrWjow1i9Vw80XFpaPWBaksnYUmSZkv',
+  token: '',
 });
 
 async function uploadImageToSanity(imageUrl) {
